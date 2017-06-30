bdebca963814bfbcdd96049b819c294eee8286d4
sijmenhuizenga <sijmenhuizenga@gmail.com>
2017-06-30 17:39:18 +0200

    add backend connection

---
+++ www/js/main.js
@@ -0,0 +1,28 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    ws = new WebSocket("ws://35.157.253.195/api");
+    //ws = new WebSocket("ws://localhost:8090/api");
+    ws.onopen = sentLocation;
+}
+var ws;
+
+function sentLocation(){
+    navigator.geolocation.getCurrentPosition(onLocationReceived, onLocationError);
+}
+
+function onLocationReceived(position) {
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    console.log("lat : " + lat + " lng : " + lng);
+
+    ws.send(JSON.stringify({
+        type: "POS",
+        lat: lat,
+        lng: lng
+    }));
+    console.log("sent POS");
+}
+
+function onLocationError(error) {
+    console.error('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
+}

34ff5852e48ff2801157406a955a04fc974e63de
sijmenhuizenga <sijmenhuizenga@gmail.com>
2017-06-30 20:29:55 +0200

    add db connection

---
+++ src/main/java/it/sijmen/bieb/DB.java
@@ -0,0 +1,35 @@
+package it.sijmen.bieb;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Created by Sijmen on 30-6-2017.
+ */
+public class DB {
+    
+    private static Connection connection;
+
+    public static Connection openConn() throws SQLException {
+        if(connection != null && !connection.isClosed())
+            return connection;
+        try {
+            Class.forName("org.postgresql.Driver");
+            return connection = DriverManager.getConnection("jdbc:postgresql://35.157.253.195:25432/BIEB", "docker", "docker");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            throw new SQLException(e.getMessage());
+        }
+    }
+    
+    public static void create(String query, String... args) throws SQLException {
+        Connection connection = openConn();
+        Statement statement = connection.createStatement();
+        statement.execute(query, args);
+    }
+    
+
+}
--- src/main/java/it/sijmen/bieb/WebsocketServer.java
+++ src/main/java/it/sijmen/bieb/WebsocketServer.java
@@ -1,10 +1,18 @@
 package it.sijmen.bieb;
 
-import org.eclipse.jetty.websocket.api.*;
-import org.eclipse.jetty.websocket.api.annotations.*;
-import java.io.*;
-import java.util.*;
-import java.util.concurrent.*;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * Created by Sijmen on 30-6-2017.
@@ -13,7 +21,8 @@ import java.util.concurrent.*;
 public class WebsocketServer {
 
     private static final Queue<Session> sessions = new ConcurrentLinkedQueue<>();
-
+    private Gson gson = new Gson();
+    
     @OnWebSocketConnect
     public void connected(Session session) {
         System.out.println("CONNECTED: " + session.getRemoteAddress());
@@ -30,6 +39,30 @@ public class WebsocketServer {
     public void message(Session session, String message) throws IOException {
         System.out.println("RECEIVED: " + message);
         session.getRemote().sendString(message);
+
+        JsonParser parser = new JsonParser();
+        JsonObject o = parser.parse(message).getAsJsonObject();
+        
+        String type = o.get("type").getAsString();
+        switch(type){
+            case "MELDING": melding(o);
+        }
+        
     }
-    
+
+    private void melding(JsonObject o) {
+        String lat = o.get("lat").getAsString();
+        String lng = o.get("lng").getAsString();
+        String opmerking = o.get("opmerking").getAsString();
+        String sterkte = o.get("sterkte").getAsString();
+
+        try {
+            DB.create("INSERT INTO meldingdata (melding_opermerking, waarde_meting, lat, lon) " +
+                    "VALUES (?, ?, ?, ?)", opmerking, sterkte, lat, lng);
+        } catch (SQLException e) {
+            System.err.println("Niet gelukt melding op te slaan in database.");
+            e.printStackTrace();
+        }
+    }
+
 }

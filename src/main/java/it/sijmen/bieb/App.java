87ca7fcf940064b441a9ee9eb159e7abb3f94fff
sijmenhuizenga <sijmenhuizenga@gmail.com>
2017-06-30 17:19:40 +0200

    Initial working backend app

--- src/main/java/it/sijmen/bieb/App.java
+++ src/main/java/it/sijmen/bieb/App.java
@@ -1,15 +1,19 @@
 package it.sijmen.bieb;
 
-import static spark.Spark.init;
-import static spark.Spark.webSocket;
+import static spark.Spark.*;
 
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main( String[] args ) {
+    public static void main(String[] args){
+        port(80);
         webSocket("/api", WebsocketServer.class);
+        get("/ping", (request, response) -> {
+            System.out.println("PING");
+            return "pong";
+        });
         init();
     }
 }
